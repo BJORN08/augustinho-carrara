dd00bc0754398f0a330157ec2a7961e8db8e8d91
BJORN08 <160598153+BJORN08@users.noreply.github.com>
2024-11-04 11:54:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
+)
+
+criaCartao(
+    'Filme',
+    'Qual o filme com maior bilheteria do mundo',
+    'Avatar'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual ano foi lançado Minecraft?',
+    '2011'
+)
+
+criaCartao(
+    'Matemática II',
+    'Qual aplicativo utilizamos nas aulas de Programação - Matemática II?',
+    'Visual Studio Code'
+)
+
+criaCartao(
+    'Musculação',
+    'O que é musculação?',
+    'A musculação é uma atividade física de força em que são utilizados equipamentos, como barras, polias e halteres. A combinação dos exercícios, aceleração do metabolismo, além de promover o ganho de massa muscular:.'
+)
+criaCartao(
+    'creatina' ,
+    'Oque é creatina?' ,
+    ' A creatinina é um produto da degradação da fosfocreatina no músculo, e é geralmente produzida em uma taxa praticamente constante pelo corpo — taxa diretamente proporcional à massa muscular da pessoa:.'
+)
